e6c384a37654b51bafd843356621a2aa911dc263
Sakthipriyan Vairamani (thefourtheye) <thechargingvolcano@gmail.com>
2017-08-07 00:16:22 -0700

    introduce TOC and clickable links

--- mailer.py
+++ mailer.py
@@ -1,5 +1,7 @@
 import smtplib
+import re
 from email.mime.text import MIMEText
+
 try:
     import simplejson as json
 except ImportError:
@@ -17,28 +19,44 @@ def get_color_based_on_severity(severity):
     return 'green'
 
 
+def get_html_name(name):
+    return re.sub("\s+", "_", name)
+
+
 def format_item(item):
     return """
     <p>
-      <table width='100%' border=0 cellpadding=12 cellspacing=0>
+      <table width='100%' border=0 cellpadding=12 cellspacing=0 name="{3}">
         <tr style='background-color:{1}'>
           <td align='center' style='font-size:18px;border:0'><b>{0}</b></td>
+          <td style='width: 5%'><a href="#toc">Top</a></td>
         </tr>
         <tr>
-          <td style='border:0'>{2}</td>
+          <td style='border:0'><pre>{2}</pre></td>
         </tr>
       </table>
     </p>
     """.format(
         item["title"],
         get_color_based_on_severity(item["severity"]),
-        item["body"]
+        item["body"],
+        get_html_name(item["title"])
     )
+
+
+def build_toc(content):
+    result = "<div id='toc' name='toc'><h3>Table of Contents</h3><ul><li>"
+    result += "\n<li>".join(
+        map(lambda c: '<a href="#{0}" />{1}</a>'.format(get_html_name(c["title"]), c["title"]), content)
+    )
+    result += "</ul></div>"
+    return result
 
 
 def mail(cluster, content):
     html = "<center><h1>{0} cluster monitoring alert</h1></center>".format(cluster) + \
-        "<br />".join(map(format_item, content))
+           build_toc(content) + \
+           "<br />".join(map(format_item, content))
     msg = MIMEText(html, "html")
 
     if any(c["severity"] == "FATAL" for c in content):
