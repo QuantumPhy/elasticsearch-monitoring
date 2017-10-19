a6cda0bddad237f9325018242fab1a0c81f3441f
Sakthipriyan Vairamani (thefourtheye) <thechargingvolcano@gmail.com>
2017-10-19 15:35:32 +0530

    monitor oversized shards

--- shards.py
+++ shards.py
@@ -1,6 +1,6 @@
-from indices import is_white_listed
-import logging
 import re
+
+from indices import is_white_listed
 from tabularize_json import tabularize
 
 try:
@@ -9,22 +9,26 @@ except ImportError:
     import json
 
 
-def sort(item):
+def sort_by_index(item):
     return item["index"], int(item["shard"]), item["prirep"], item["node"]
 
 
-def table(title, l):
+def sort_by_size(item):
+    return -int(item["store"] or '0'), item["index"], int(item["shard"]), item["prirep"], item["node"]
+
+
+def table(title, l, sorter=None):
     temp = """
         <table width='100%' border=1 cellpadding=3 cellspacing=0>
         <caption>{0} Shards</caption>
-        <tr><th>Index</th><th>Shard</th><th>prirep</th><th>Reason</th><th>Docs</th><th>Store</th><th>IP</th><th>Node</th></tr>
+        <tr><th>Index</th><th>Shard</th><th>prirep</th><th>Reason</th><th>Docs</th><th>Size (GB)</th><th>IP</th><th>Node</th></tr>
     """.format(title)
-    for item in sorted(l, key=sort):
+    for item in sorted(l, key=sorter):
         temp += "<tr><td>" + "</td><td>".join([
             item["index"],
             item["shard"],
             item["prirep"],
-            item["unassigned.reason"] or "",
+            item["unassigned.reason"] or "N/A",
             item["docs"] or '0',
             item["store"] or '0',
             item["ip"] or "",
@@ -34,7 +38,7 @@ def table(title, l):
 
 
 def shards(connection, config, health):
-    r1 = connection("/_cat/shards?h=index,shard,prirep,state,unassigned.reason,docs,store,ip,node")
+    r1 = connection("/_cat/shards?bytes=g&h=index,shard,prirep,state,unassigned.reason,docs,store,ip,node")
     response = r1.read()
     result = {
         "severity": "INFO",
@@ -49,6 +53,7 @@ def shards(connection, config, health):
         }
     else:
         shards_data = json.loads(response)
+        oversize_limit = config.get("shard_size_limit", 50)  # Giga Bytes
 
         cfg_whitelisted_indices = config.get("whitelisted_indices", [])
         whitelisted_indices = set(map(lambda x: re.compile(x), cfg_whitelisted_indices))
@@ -58,6 +63,7 @@ def shards(connection, config, health):
         init = [shard for shard in shards_data if shard["state"] == "INITIALIZING"]
         relocating = [shard for shard in shards_data if shard["state"] == "RELOCATING"]
         unassigned = [shard for shard in shards_data if shard["state"] == "UNASSIGNED"]
+        oversized = [shard for shard in shards_data if int(shard["store"]) > oversize_limit]
 
         if init and not all(is_index_whitelisted(i["index"]) for i in init):
             result["severity"] = "WARNING" if health != "FATAL" else "FATAL"
@@ -65,6 +71,8 @@ def shards(connection, config, health):
             result["severity"] = "WARNING" if health != "FATAL" else "FATAL"
         elif unassigned and not all(is_index_whitelisted(i["index"]) for i in unassigned):
             result["severity"] = "WARNING" if health != "FATAL" else "FATAL"
+        elif oversized:
+            result["severity"] = "WARNING"
 
         result["body"] += """<table width='100%' border=1 cellpadding=3 cellspacing=0>
             <tr><td>Total</td><td>{0}</td></tr>
@@ -72,19 +80,22 @@ def shards(connection, config, health):
             <tr><td>Initializing</td><td>{2}</td></tr>
             <tr><td>Relocating</td><td>{3}</td></tr>
             <tr><td>Unassigned</td><td>{4}</td></tr>
+            <tr><td>Oversized</td><td>{4}</td></tr>
         </table><br />""".format(
             len(shards_data),
             len(started),
             len(init),
             len(relocating),
-            len(unassigned)
+            len(oversized)
         )
 
         if unassigned:
-            result["body"] += table("Unassigned", unassigned)
+            result["body"] += table("Unassigned", unassigned, sort_by_index)
         if init:
-            result["body"] += table("Initializing", init)
+            result["body"] += table("Initializing", init, sort_by_index)
         if relocating:
-            result["body"] += table("Relocating", relocating)
+            result["body"] += table("Relocating", relocating, sort_by_index)
+        if oversized:
+            result["body"] += table("Oversized", oversized, sort_by_size)
 
     return result
